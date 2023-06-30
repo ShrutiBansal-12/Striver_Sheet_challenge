84852b7d4b500021ee55c25fa7b040b828ea77e3
ShrutiBansal-12 <bansal2001s@gmail.com>
2023-06-30 19:15:26 +0530

    Update Majority_Element.cpp

--- Majority_Element.cpp
+++ Majority_Element.cpp
@@ -42,3 +42,25 @@ public:
         return ans;
     }
 };
+
+
+//---------------Approach 3----------------//
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0;
+        int candidate=0;
+        for(int num:nums){
+            if(count==0){
+                candidate=num;
+            }
+            if(num==candidate){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return candidate;
+    }
+};
