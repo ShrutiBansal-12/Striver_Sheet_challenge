18f83358a0c6646480a6af0a5b55928e1fc2dd91
ShrutiBansal-12 <bansal2001s@gmail.com>
2023-06-30 16:58:34 +0530

    Add files via upload

---
+++ Majority_Element_II.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n=nums.size(); 
+        int size=n/3;
+        vector <int> ans;
+        vector <int> unique;
+        sort(nums.begin(),nums.end());
+        int i=0;
+        for(i;i<n-1;i++){
+            if(nums[i]!=nums[i+1]){
+                unique.push_back(nums[i]);
+            }
+        }
+        unique.push_back(nums[i]);
+        int s=unique.size();  
+        for(int i=0;i<s;i++){
+            int d=unique[i];
+            int c=0;
+            for(int j=0;j<n;j++){
+                if(nums[j]==d){
+                    c++;
+                }
+            }
+
+            if(c>(int)size){
+                ans.push_back(d);
+            }
+        }
+        return ans;
+    }
+};
